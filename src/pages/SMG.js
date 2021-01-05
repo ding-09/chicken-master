88c3bbd25d37c362acfa6db043ee833bd9c375ab
tiffany <yang.ding@hotmail.com>
2021-01-04 20:01:32 -0800

    forgot to add SMG section header

--- src/pages/SMG.js
+++ src/pages/SMG.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Card from '../components/Card';
+import Title from '../components/Title';
 
-const SMG = ({ weaponDataSMG }) => {
+const SMG = ({ weaponDataSMG, title }) => {
   return (
-    <section className="weapon-container">
-      {weaponDataSMG &&
-        weaponDataSMG.map((weaponData, index) => (
-          <Card weaponData={weaponData} key={index} />
-        ))}
+    <section>
+      {title && <Title title={title} />}
+      <section className="weapon-container">
+        {weaponDataSMG &&
+          weaponDataSMG.map((weaponData, index) => (
+            <Card weaponData={weaponData} key={index} />
+          ))}
+      </section>
     </section>
   );
 };
